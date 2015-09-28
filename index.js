e23280fb07bfa32796ca5bbbd19f81b450b3b8c7
AndiOxidant <andi.oxidant@noname-media.com>
2015-09-29 00:27:45 +0200

    Fig

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ var spawn = require('child_process').spawn;
 module.exports = function(coffeeBreak) {
 	"use strict";
 	
-	coffeeBreak.registerTask('prepare', function(conf, logger, done) {
-		logger.info('Running grunt task');
+	coffeeBreak.registerTask('prepare', function(project, data, done) {
+		coffeeBreak.debug('Running grunt task');
 
-		if (!conf.grunt) {
+		if (!project.grunt) {
 			//No grunt build required, skipping
 			done();
 			return;
 		}
 
-		if (conf.grunt && conf.grunt.task) {
-			var task = Array.isArray(conf.grunt.task) ? conf.grunt.task : [conf.grunt.task];
+		if (project.grunt && project.grunt.task) {
+			var task = Array.isArray(project.grunt.task) ? project.grunt.task : [project.grunt.task];
 			var grunt = spawn('grunt', task);
 
 			grunt.stdout.on('data', function(data) {
-				logger.info('grunt: ' + data.toString().replace(/\n$/, ''));
+				coffeeBreak.debug('grunt: ' + data.toString().replace(/\n$/, ''));
 			});
 
 			grunt.stderr.on('data', function() {
-				logger.info('grunt-err: ' + data.toString().replace(/\n$/, ''));
+				coffeeBreak.debug('grunt-err: ' + data.toString().replace(/\n$/, ''));
 			});
 
 			grunt.on('close', function(code) {
